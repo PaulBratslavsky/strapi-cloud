6442e60c87e65ef6cfa57c2fee32e1e28cad0501
paul bratslavsky <paulbratslavsky@pauls-Mac-mini.local>
2023-02-14 14:12:00 -0600

    stuff

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,14 +2,14 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      host: env('DB_HOST', '127.0.0.1'),
+      port: env.int('DB_PORT', 5432),
+      database: env('DB_NAME', 'strapi'),
+      user: env('DB_USERNAME', 'strapi'),
+      password: env('DB_PASSWORD', 'strapi'),
+      schema: env('DB_SCHEMA', 'public'), // Not required
       ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        rejectUnauthorized: env.bool('DB_SSL_SELF', false), // For self-signed certificates
       },
     },
     debug: false,
